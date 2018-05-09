929f6aef7c5e3686bb9a4a2952258e036e2e366a
Jeff Dyer <jeffdyer@acm.org>
2018-05-09 08:26:58 -0700

    Init L104

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /*
-   L000 compiler service.
+   L104 compiler service.
    @flow weak
 */
 const https = require("https");
 const express = require('express')
 const app = express();
-const langID = "000";
+const langID = "104";
 // SHARED START
 const compiler = require("./lib/compile.js");
 app.set('port', (process.env.PORT || "5" + langID));
