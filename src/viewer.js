c3ddb5b8b330bb1d8a3ab7628db1182e0712bb38
Jeff Dyer <jeffdyer@acm.org>
2020-04-01 10:30:50 -0700

    Fix top grid line

--- src/viewer.js
+++ src/viewer.js
@@ -925,7 +925,7 @@ window.gcexports.viewer = (function () {
                 },
               },
               min: min,
-              max: max + (max < 10 && max ||max * .1),
+              max: max < 10 && max || max + max * .1,
             },
           },
           grid: {
